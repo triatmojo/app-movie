c83b9e952ecce0a5faf43e75cfe6f8cdc0e65523
Tri <tri@users-MacBook-Pro.local>
2023-06-22 20:23:40 +0700

    blade templating using admminlte

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'admin.dashboard');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
